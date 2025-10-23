961e7a9c501195daace0457a871d4b78325db326
2030Gian <gian.arteaga@utec.edu.pe>
2025-10-23 15:22:50 -0500

    a

--- backend/microservicios/tutorai-be-core/src/main/java/com/example/tutoraibecore/subtemas/dominio/SubtemasRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.example.tutoraibecore.subtemas.dominio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SubtemasRepositorio extends JpaRepository<Subtema, UUID> {
-}
